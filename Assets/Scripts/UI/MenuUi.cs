642901ba7e451b7004b73d6956a797ba44d76ca1
Ivan Popenya <54358431+Landfix@users.noreply.github.com>
2023-05-28 13:38:54 +0300

    realization menu

---
+++ Assets/Scripts/UI/MenuUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Systems;
+using Sounds_container;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MenuUi : MonoBehaviour
+    {
+        [SerializeField] private SoundEffect _soundEffect;
+
+        [Header("Components")]
+        [SerializeField] private TrophyElement _trophyElement;
+        [SerializeField] private SoundButton _soundButton;
+        [SerializeField] private Button _playBtn;
+
+        private Preferences _preferences;
+        
+        private void Start()
+        {
+            _preferences = GlobalManager.I.Preferences;
+            _playBtn.onClick.AddListener(OnClickPlay);
+            _trophyElement.Initialize(_preferences);
+            _soundButton.Initialize(_preferences,_soundEffect);
+            _soundButton.ChangedSound += GlobalManager.I.MusicEffect.IsPlaybackMusic;
+        }
+
+        private void OnClickPlay()
+        {
+            _soundEffect.PlayClip();
+            _playBtn.onClick.RemoveListener(OnClickPlay);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private void OnDestroy()
+        {
+            _playBtn.onClick.RemoveListener(OnClickPlay);
+            _soundButton.ChangedSound -= GlobalManager.I.MusicEffect.IsPlaybackMusic;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SoundButton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Systems;
+using Sounds_container;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class SoundButton : MonoBehaviour
+    {
+        [SerializeField] private Button _soundBtn;
+        [Header("Sprites")]
+        [SerializeField] private Sprite _soundSprite;
+        [SerializeField] private Sprite _withoutSoundSprite;
+
+        private bool _isSoundPlayback;
+        private SoundEffect _soundEffect;
+        private Preferences _preferences;
+
+        public event Action<bool> ChangedSound;
+        
+        public void Initialize(Preferences preferences, SoundEffect soundEffect)
+        {
+            _soundEffect = soundEffect;
+            _preferences = preferences;
+            _soundBtn.onClick.AddListener(OnClickSound);
+            _isSoundPlayback = _preferences.soundPlayback == 1;
+            _soundBtn.image.sprite = _isSoundPlayback ? _soundSprite : _withoutSoundSprite;
+            _preferences.SwitchedSoundByButton += SwitchedSoundByButton;
+        }
+
+        private void SwitchedSoundByButton(bool isActivate)
+        {
+            if (isActivate)
+            {
+                _isSoundPlayback = true;
+                _soundBtn.image.sprite = _soundSprite;
+                ChangedSound?.Invoke(isActivate);
+            }
+            else
+            {
+                _isSoundPlayback = false;
+                _soundBtn.image.sprite = _withoutSoundSprite;
+                ChangedSound?.Invoke(isActivate);
+            }
+        }
+
+        private void OnClickSound()
+        {
+            if(_isSoundPlayback)
+                _soundEffect.PlayClip();
+            
+            _isSoundPlayback = !_isSoundPlayback;
+            _soundBtn.image.sprite = _isSoundPlayback ? _soundSprite : _withoutSoundSprite;
+            ChangedSound?.Invoke(_isSoundPlayback);
+            _preferences.SetSoundPlayback(_isSoundPlayback ? 1 : -1);
+        }
+
+        private void OnDestroy() => 
+            _preferences.TurnedOffSoundToAds -= SwitchedSoundByButton;
+    }
+}
